3c9c029635ca7c26c0df670e235a9bc093a20c9b
alekart <94320656+DiabloPro@users.noreply.github.com>
2022-06-05 09:23:20 -0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,12 +1,15 @@
 local UiLibrary = {}
 UiLibrary.__index = UiLibrary
 
+local folder = game:GetObjects("rbxassetid://9820680667")[1]
+local colorable = {}
+local theme = Color3.fromRGB(255,255,255)
+local selected
+
 function UiLibrary.init(name)
-    local folder = game:GetObjects("rbxassetid://9820680667")[1]
 	local screenGUI = folder.ScreenGui:Clone()
 	local menu = screenGUI.Menu
 	menu.TopBar.Title.Text = name
-	local colorable = {}
 	table.insert(colorable, menu.Border)
 	table.insert(colorable, menu.Tabs.Border)
 	table.insert(colorable, menu.Body.Border)
@@ -25,24 +28,22 @@ function UiLibrary.init(name)
 	end)
 
 	return setmetatable({
-		folder = folder,
 		screenGUI = screenGUI,
-        colorable = colorable,
 		selected = selected
 	}, UiLibrary)
 end
 
-local tab = {}
-tab.__index = tab
+local tabs = {}
+tabs.__index = tabs
 
 function UiLibrary:createTab(image)
-    local tab = self.folder.Tab:Clone()
+    local tab = folder.Tab:Clone()
 	tab.Parent = self.screenGUI.Menu.Tabs.Tab
 	tab.Tab.Image = image
-	local window = self.folder.Window:Clone()
+	local window = folder.Window:Clone()
 	window.Parent = self.screenGUI.Menu.Body
 
-	if not self.selected then
+	if not selected then
 		selected = tab
 		selected.Visible = true
 	end
@@ -50,31 +51,58 @@ function UiLibrary:createTab(image)
 	tab.Tab.MouseButton1Click:Connect(function()
 		selected.Visible = false
 		selected = tab
-		selected.Visbile = true
+		selected.Visible = true
 	end)
+
 	return setmetatable({
 		window = window,
 		getLongestSide = function()
-			if self.window.Left.ListLayout.AbsoluteContentSize.Y >= self.window.Right.ListLayout.AbsoluteContentSize.Y then
-				return self.window.Left
+			if window.Left.ListLayout.AbsoluteContentSize.Y >= window.Right.ListLayout.AbsoluteContentSize.Y then
+				return window.Left
 			else
-				return self.window.Right
+				return window.Right
 			end
 		end
-	}, tab)
+	}, tabs)
 end
 
 local objects = {}
 objects.__index = objects
 
-function tab:createSection(name)
-	local section = self.folder.Section:Clone()
+function tabs:createSection(name)
+	local section = folder.Section:Clone()
 	section.Parent = self.getLongestSide()
 	section.Title.Frame.TextLabel.Text = name
-	section.Title.Size.X.Offset = section.Title.Frame.TextLabel.TextBounds.X
+	section.Title.Size = Udim2.new(0, section.Title.Frame.TextLabel.TextBounds.X, 0, 20) --[[[complete it]]--
 	return setmetatable({
-		section = section
+		section = section,
 	}, objects)
+end
+
+function objects:createButton(name, callBack)
+	local button = folder.Button:Clone()
+	button.Parent = self.section
+	button.Title.Text = name
+
+	button.MouseButton1Down:Connect(function()
+		button.BackgroundColor3 = theme
+	end)
+
+	button.MouseButton1Up:Connect(function()
+		button.BackgroundColor3 = Color3.fromRGB(25,25,25)
+	end)
+
+	Button.MouseLeave:Connect(function()
+		button.BackgroundColor3 = Color3.fromRGB(25,25,25)
+	end)
+
+	button.MouseButton1Click:Connect(function()
+		callBack()
+	end)
+end
+
+function objects:createToggle(name, keybindable, callBack)
+	
 end
 
 return UiLibrary
